90c603fd8cec7f23f5694ebdb8c083ff47bea306
Nehleh <kargarfard870@gmail.com>
2022-01-27 09:42:47 +1100

    add gubbins

--- bin/cmpTree_plot.py
+++ bin/cmpTree_plot.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 cmpTrees = args.cmpTreesResult
 
-# cmpTrees = '/home/nehleh/PhiloBacteria/Summary_Results/all_cmpTrees.result'
+# cmpTrees = '/home/nehleh/PhiloBacteria/hpc/nu_09/Summary_Results/all_cmpTrees.result'
 
 
 f = open(cmpTrees, "r")
@@ -24,13 +24,13 @@ df = pd.read_csv(f,sep='\t', names=['No', 'clonalTree','otherTrees', 'Quartet' ,
 new_df= df.loc[df['otherTrees'].between('0' , '4' , inclusive=False )].apply(pd.to_numeric)
 new_df['otherTrees'].replace(to_replace=[1,2,3],value=['CFML','Gubbins','PB2'] ,inplace=True)
 # print(new_df)
-g_df = new_df.loc[(new_df['otherTrees'] != 'Gubbins') ]
+# g_df = new_df.loc[(new_df['otherTrees'] != 'Gubbins') ]
 
 
 
 
 
-fig = plt.figure(figsize=(10,10))
+fig = plt.figure(figsize=(10,5))
 
 # ax1 = fig.add_subplot(2, 4, 1)
 # ax1 = sns.boxplot(x = 'otherTrees', y="Quartet" ,data=new_df)
@@ -57,14 +57,14 @@ fig = plt.figure(figsize=(10,10))
 # ax5 = sns.stripplot(x = 'otherTrees', y="UMAST" ,data=new_df,  jitter=True, dodge=True, marker='o', color=".1")
 # ax5.set_title('UMAST' , fontsize=9)
 
-ax6 = fig.add_subplot(2, 2, 1)
-ax6 = sns.boxplot(x = 'otherTrees', y="RFWeighted" ,data=g_df )
-ax6 = sns.stripplot(x = 'otherTrees', y="RFWeighted" ,data=g_df,  jitter=True, dodge=True, marker='o', color=".1")
+ax6 = fig.add_subplot(1, 2, 1)
+ax6 = sns.boxplot(x = 'otherTrees', y="RFWeighted" ,data=new_df )
+ax6 = sns.stripplot(x = 'otherTrees', y="RFWeighted" ,data=new_df,  jitter=True, dodge=True, marker='o', color=".1")
 ax6.set_title('RFWeighted' , fontsize=9)
 
-ax7 = fig.add_subplot(2, 2, 2)
-ax7 = sns.boxplot(x = 'otherTrees', y="GeoUnrooted" ,data=g_df )
-ax7 = sns.stripplot(x = 'otherTrees', y="GeoUnrooted" ,data=g_df,  jitter=True, dodge=True, marker='o', color=".1")
+ax7 = fig.add_subplot(1, 2, 2)
+ax7 = sns.boxplot(x = 'otherTrees', y="GeoUnrooted" ,data=new_df )
+ax7 = sns.stripplot(x = 'otherTrees', y="GeoUnrooted" ,data=new_df,  jitter=True, dodge=True, marker='o', color=".1")
 ax7.set_title('GeoUnrooted' , fontsize=9)
 
 
